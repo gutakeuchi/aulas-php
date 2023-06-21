cce352df6741a91310a669ebff2c14b21dabd088
gutakeuchi <gustavo.takeuchi87@gmail.com>
2023-06-21 20:18:41 -0300

    atualizando arquivo

---
+++ estruturas_de_controle/1_estrutura_if/index.php
@@ -0,0 +1,32 @@
+<?php
+
+ // Condicão verdadeira
+ if(5 > 2){
+    echo "Deu certo! Entro no if <br>";
+ }
+  // Condição falsa
+
+ if(2 >= 5) {
+    echo "Não vai entrar no if, porque deu false! <br>";
+ }
+ 
+ // utilizar op. lógico
+
+ if(10 === 10 && 9 > 3 ) {
+    echo "Deu certo! Entro no if 2<br>";
+ }
+
+ // variáveis 
+
+ $a = 10;
+ $b = 5;
+
+ $c = "Deu certo! Entrou no if 3 <br>";
+
+ if($a >= $b) 
+ {
+    echo $c;
+ }
+
+
+
---
+++ estruturas_de_controle/5_switch/index.php
@@ -0,0 +1,33 @@
+<?php
+
+ $x = 5;
+
+ switch($x) {
+
+    case 0 :
+        echo "X é igual a 0 <br>";
+        break;
+    case 1 :
+        echo "X é igual a 1 <br>";
+        break;
+    default:
+        echo "X não é igual a nenhum dos valores mencionados <br>";
+ }
+
+ $y = "";
+
+ switch($y) {
+
+    case "Gustavo":
+        echo "Meu nome é Gustavo <br>";
+        break;
+    case "Matheus":
+        echo "Meu nome é Matheus <br>";
+        break;
+    default:
+        echo "O nome não foi encontrado <br>";
+ }
+
+ 
+
+ 
